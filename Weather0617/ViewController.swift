cf05bf8fc7b9616c24812ba6713d133ef98822f7
Kirill Lukyanov <kirillko@MacBook-Pro-Kirill.local>
2017-07-03 20:13:44 +0300

    Initial Commit

---
+++ Weather0617/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Weather0617
+//
+//  Created by Kirill Lukyanov on 03.07.17.
+//  Copyright © 2017 Kirill Lukyanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
